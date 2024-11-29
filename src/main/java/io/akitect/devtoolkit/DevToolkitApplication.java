f60c05edc7fc454ff3e9e5fd52a3267bc9fb26bd
DUY TRAN <akitect.io@gmail.com>
2024-11-29 11:44:07 +0700

    add start

---
+++ src/main/java/io/akitect/devtoolkit/DevToolkitApplication.java
@@ -0,0 +1,12 @@
+package io.akitect.devtoolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevToolkitApplication.class, args);
+    }
+}
